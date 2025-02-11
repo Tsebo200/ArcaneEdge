79bbc4b8559239012ec1e256ce2dc2fb5563a4c2
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2025-02-11 16:30:13 +0200

    Set up the bottom/card section

--- client/src/Pages/Dashboard/Dashboard.jsx
+++ client/src/Pages/Dashboard/Dashboard.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavBar from '../../Components/NavBar/NavBar'
-import Hero from '../../Components/Hero/Hero'
+import Hero from '../../Components/Sections/Hero/Hero'
+import CardSection from '../../Components/Sections/CradSection/CardSection'
 
 function Dashboard() {
   return (<>
 
     <NavBar/>
     <Hero/>
+    <CardSection/>
     </>
 
   )
