4cecfa71fc05d32c060c6e8b398ff12591f19642
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2025-02-13 00:58:22 +0200

    Website Version 2 Complete

--- client/src/Components/Sections/BottomSection/ContactSection/ContactSection.jsx
+++ client/src/Components/Sections/BottomSection/ContactSection/ContactSection.jsx
@@ -8,6 +8,10 @@ function ContactSection() {
 
         <div className={styles.contactHead}> Contact</div>
         <div className={styles.contactBody}>Ready to transform your business? Let’s work together!</div>
+
+        <div className={styles.emailText}><b>Email:</b> info@arcaneedge.co.za</div>
+        <div className={styles.websiteText}><b>website:</b> www.arcaneedge.co.za</div>
+
         <div className={styles.getInTouch}>
           <div className={styles.getInTouchText}>Get In Touch</div>
         </div>
