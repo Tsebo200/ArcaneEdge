641ac3eb0606ebe061b3f8dd02f54e7e3a6bd056
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2025-02-12 20:53:02 +0200

    Social Media Icons

--- client/src/Components/Sections/BottomSection/ContactSection/ContactSection.jsx
+++ client/src/Components/Sections/BottomSection/ContactSection/ContactSection.jsx
@@ -3,7 +3,31 @@ import styles from '../ContactSection/ContactSection.module.scss'
 
 function ContactSection() {
   return (
-    <div>ContactSession</div>
+    <>
+    <div className={styles.mainContainer}>
+
+        <div className={styles.contactHead}> Contact</div>
+        <div className={styles.contactBody}>Ready to transform your business? Let’s work together!</div>
+        <div className={styles.getInTouch}>
+          <div className={styles.getInTouchText}>Get In Touch</div>
+        </div>
+
+        <div className={styles.iconContainer}>
+          <div className={styles.xContainer}>
+            <div className={styles.xIcon}></div>
+            </div>
+          <div className={styles.pinterestContainer}>
+          <div className={styles.pinterestIcon}></div>
+          </div>
+          <div className={styles.linkedInContainer}>
+          <div className={styles.linkedInIcon}></div>
+          </div>
+        </div>
+
+        {/* <div className="iconContainer}></div> */}
+
+    </div>
+    </>
   )
 }
 
