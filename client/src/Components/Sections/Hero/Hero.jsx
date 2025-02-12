ae89dfb9de4d6ec4f765cbb3ebcfa680f9122b5c
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2025-02-13 00:06:55 +0200

    Dashboard complete
    Video added, unfortunately autoplay has been terminated on every search engine unless you mute the video as well

--- client/src/Components/Sections/Hero/Hero.jsx
+++ client/src/Components/Sections/Hero/Hero.jsx
@@ -1,10 +1,50 @@
 import React from "react";
 import styles from "./Hero.module.scss";
+import VideoLink from "../../../Assets/ArcaneEdge_Vid.mp4"
 
 function Hero() {
+
+
+  // const videoRef = React.useRef(null);
+
+  // React.useEffect(() => {
+  //   // Attempt to play when component mounts
+  //   const playVideo = async () => {
+  //     try {
+  //       await videoRef.current.play();
+  //     } catch (err) {
+  //       console.log("Autoplay was prevented:", err);
+  //       // Video will show controls and user can play manually
+  //     }
+  //   };
+    
+  //   playVideo();
+  // }, []);
+
+
+
   return (
     <>
       <div className={styles.mainContainer}>
+      {/* <video src={'https://youtu.be/moSKhf-566A?si=cEZ-D-TwNrk1BV7o'} width="600" height="300" controls="controls" autoplay="true" /> */}
+      {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/moSKhf-566A?si=cEZ-D-TwNrk1BV7o&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> */}
+        {/* <video className="demoVid" width="640" height="264" controls src="../../../../public/ArcaneEdge_Vid.mp4"></video>  */}
+        <div className={styles.videoContainer}>
+    <video 
+      className={styles.responsiveVideo}
+      // ref={videoRef}
+      src={VideoLink} 
+      autoPlay 
+      loop
+      playsInline
+      controls
+    />
+  </div>
+
+
+
+
+  <div className={styles.contentOverlay}>
         <div className={styles.allHeroComps}>
           <div className={styles.allTextContainer}>
             <div className={styles.headerContainer}>
@@ -32,6 +72,7 @@ function Hero() {
                 </div>
               </div>
           </div>
+        </div>
         </div>
       </div>
     </>
