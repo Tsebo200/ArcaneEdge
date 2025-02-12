284edb06059e73220a37ca5981a86aee1295a253
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2025-02-13 00:21:27 +0200

    Cleaning up and adding hoverstates
    adding hover state to social icons

--- client/src/Components/Sections/Hero/Hero.jsx
+++ client/src/Components/Sections/Hero/Hero.jsx
@@ -30,7 +30,10 @@ function Hero() {
       {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/moSKhf-566A?si=cEZ-D-TwNrk1BV7o&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> */}
         {/* <video className="demoVid" width="640" height="264" controls src="../../../../public/ArcaneEdge_Vid.mp4"></video>  */}
         <div className={styles.videoContainer}>
-    <video 
+
+{/* Uncomment for video */}
+
+    {/* <video 
       className={styles.responsiveVideo}
       // ref={videoRef}
       src={VideoLink} 
@@ -38,7 +41,10 @@ function Hero() {
       loop
       playsInline
       controls
-    />
+    /> */}
+
+
+
   </div>
 
 
