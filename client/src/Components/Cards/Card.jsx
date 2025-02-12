c371729ac6f6a236eb054ac5b0437a06cc043407
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2025-02-12 18:39:54 +0200

    Cards Complete

--- client/src/Components/Cards/Card.jsx
+++ client/src/Components/Cards/Card.jsx
@@ -6,20 +6,63 @@ function Card() {
     <>
     <div className={styles.cardOne}>
         <div className={styles.imgContainer}></div>
-        <div className={styles.aboutUsHeading}>About</div>
+        <div className={styles.aboutUsHeading}>About Us</div>
         <div className="aboutUsContainer">
-
             <div className={styles.aboutUsText}>
             ArcaneEdge is a forward-thinking technology 
             and marketing agency that bridges the gap between
             creativity and innovation.
             </div>
         </div>
-     
         <div className={styles.learnBtn}>
-        <div className={styles.learnBtnText}></div>
+        <div className={styles.learnBtnText}>Learn More</div>
         </div>
+    </div>
 
+    <div className={styles.cardTwo}>
+        <div className={styles.imgContainer}></div>
+        <div className={styles.aboutUsHeading}>Why Choose Us</div>
+        <div className="aboutUsContainer">
+            <div className={styles.aboutUsText}>
+            ArcaneEdge is a forward-thinking technology 
+            and marketing agency that bridges the gap between
+            creativity and innovation.
+            </div>
+        </div>
+        <div className={styles.learnBtn}>
+        <div className={styles.learnBtnText}>Learn More</div>
+        </div>
+    </div>
+
+    <div className={styles.cardThree}>
+        <div className={styles.imgContainer}></div>
+        <div className={styles.aboutUsHeading}>Technology Solution</div>
+        <div className="aboutUsContainer">
+            <div className={styles.aboutUsText}>
+            ArcaneEdge is a forward-thinking technology 
+            and marketing agency that bridges the gap between
+            creativity and innovation.
+            </div>
+        </div>
+        <div className={styles.learnBtn}>
+        <div className={styles.learnBtnText}>Learn More</div>
+        </div>
+    </div>
+
+
+    <div className={styles.cardFour}>
+        <div className={styles.imgContainer}></div>
+        <div className={styles.aboutUsHeading}>Marketing Solution</div>
+        <div className="aboutUsContainer">
+            <div className={styles.aboutUsText}>
+            ArcaneEdge is a forward-thinking technology 
+            and marketing agency that bridges the gap between
+            creativity and innovation.
+            </div>
+        </div>
+        <div className={styles.learnBtn}>
+        <div className={styles.learnBtnText}>Learn More</div>
+        </div>
     </div>
     </>
   )
